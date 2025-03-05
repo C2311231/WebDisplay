82370d8ddd66fc0bc4738f11d888a07fbe24ea23
Chet Severson <115961663+C2311231@users.noreply.github.com>
2025-03-04 20:37:16 -0600

    Working on RPI Build

--- pi-gen_stage/stage2/04-install-requirements/03-run.sh
+++ pi-gen_stage/stage2/04-install-requirements/03-run.sh
@@ -10,7 +10,7 @@ EOF
 
 cp files/WebDisplay.service "${ROOTFS_DIR}/etc/systemd/system"
 cp files/xorg.service "${ROOTFS_DIR}/etc/systemd/system"
-cp files/.xinitrc "${ROOTFS_DIR}/home/pi"
+cp files/.xinitrc "${ROOTFS_DIR}/home/web/.xinitrc"
 on_chroot << EOF
         chown root:root /etc/systemd/system/WebDisplay.service
         chmod 644 /etc/systemd/system/WebDisplay.service
@@ -19,7 +19,8 @@ on_chroot << EOF
         chown root:root /etc/systemd/system/xorg.service
         chmod 644 /etc/systemd/system/xorg.service
         systemctl enable xorg
-        mkdir /home/pi/WebDisplay
+        
+        mkdir /home/web/WebDisplay/
 EOF
 
 # Delete now-unnecessary custom pi-gen stuff.
@@ -29,4 +30,5 @@ EOF
 
 on_chroot << EOF
         sed -i '/SystemMaxUse/c\SystemMaxUse=10M' /etc/systemd/journald.conf
+        rm /etc/ld.so.preload
 EOF
