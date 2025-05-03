9af925fa2f336109838d2c771df252d23d4531c7
Chet Severson <115961663+C2311231@users.noreply.github.com>
2025-05-03 16:40:18 -0500

    started on devices screen

--- new_webpage_test.py
+++ new_webpage_test.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 def home():
     return render_template("/WebInterfaceV2/dashboard.html")
 
+@app.route("/devices")
+def devices():
+    return render_template("/WebInterfaceV2/devices.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
