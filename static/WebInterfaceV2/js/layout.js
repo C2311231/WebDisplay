bdee4709052b72addf4417bb469f2be4ce44e797
Chet Severson <115961663+C2311231@users.noreply.github.com>
2025-05-10 19:32:41 -0500

    Added dark mode

---
+++ static/WebInterfaceV2/js/layout.js
@@ -0,0 +1,53 @@
+console.log("Script loaded"); // Put this at the top of the file
+
+function toggleDarkMode() {
+    if (document.getElementById("color-sheet").getAttribute("href") === "/static/WebInterfaceV2/dark-colors.css") {
+        document.getElementById("color-sheet").setAttribute("href", "/static/WebInterfaceV2/colors.css");
+        document.cookie = "dark_mode=false; path=/";
+        setCookie("dark_mode", "false", 999); // Set cookie
+    }
+    else {
+        document.getElementById("color-sheet").setAttribute("href", "/static/WebInterfaceV2/dark-colors.css");
+        setCookie("dark_mode", "true", 999); // Set cookie
+    }
+
+}
+
+
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+const darkMode = getCookie("dark_mode");
+console.log("Dark mode cookie value: " + darkMode);
+
+const colorSheet = document.getElementById("color-sheet");
+if (!colorSheet) {
+    console.warn("Element with ID 'color-sheet' not found.");
+}
+
+if (darkMode === "true") {
+    colorSheet.setAttribute("href", "/static/WebInterfaceV2/dark-colors.css");
+} else if (darkMode === "false") {
+    colorSheet.setAttribute("href", "/static/WebInterfaceV2/colors.css");
+}
