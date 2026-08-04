a89582070d539c63c4d0e40b1678b56deefc29b9
WanderingDev <115961663+c2311231@users.noreply.github.com>
2026-08-03 21:29:52 -0500

    Added onboarding to main.py

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ Author: C2311231
 Notes:
 """
 
+import src.onboarding as onboarding
+
+onboarding_handler = onboarding.OnboardingHandler("TestServer", "Arch", [], ["http://localhost:8080"])
+
+onboarding_handler.start_onboarding()
+
+while True:
+    onboarding_handler.update()
+
